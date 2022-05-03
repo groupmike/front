2bafbd86ae455d726da320f659d6f088634d5120
Thomas Savournin <tosave.vbl@gmail.com>
2022-05-03 15:46:21 +0200

    Create React App structure

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+function Drawer() {
+	return (
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/tanguy">Tests Tanguy</Link>
+				</li>
+				<li>
+					<Link to="/joel">Tests Joel</Link>
+				</li>
+				<li>
+					<Link to="/nicolas">Tests Nicolas</Link>
+				</li>
+				<li>
+					<Link to="/ingrid">Tests Ingrid</Link>
+				</li>
+				<li>
+					<Link to="/thomas">Tests Thomas</Link>
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Drawer
---
+++ src/pages/Tanguy.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import HelloName from "../components/Tanguy/HelloName"
+
+function Tanguy() {
+	return <HelloName name="Tanguy" />
+}
+
+export default Tanguy
+
+// Exemple avec des props définis
+type UnComposantProps = {
+	unTexte: string
+	unTexteOptionnel?: string
+	unNombre: number
+	unTableau: []
+	unObjet: {}
+	unTableauDeNombres: number[]
+	nimporteQuoi: any
+}
+export const UnComposant = ({ unTexte }: UnComposantProps) => <>{unTexte}</>
