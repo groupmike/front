e8f6e833e883a48d43abead4e9e40c0537faf4ea
Kpoti20 <aholoujoel5@gmail.com>
2022-05-08 10:35:16 +0000

    login facebook change

--- src/components/Joel/HelloName.tsx
+++ src/components/Joel/HelloName.tsx
@@ -1,11 +1,15 @@
 import React from "react"
+import Facebook from '../Joel/Facebook'
 
 type IngridProps = {
 	name: string
 }
 
 const HelloName = ({ name }: IngridProps) => {
-	return <>Hello {name}</>
+	return <>
+			Hello {name}
+			<div><Facebook /></div>
+		   </>
 }
 
 export default HelloName
