d750bded795b5efa47979fde43072122489c1d95
Thomas Savournin <tosave.vbl@gmail.com>
2022-06-06 17:38:59 +0200

    test: utilisation du useContext du provider

--- src/pages/Thomas.tsx
+++ src/pages/Thomas.tsx
@@ -1,20 +1,34 @@
 import React from "react"
-import HelloName from "../components/Thomas/HelloName"
+import { AuthContext } from "../components/AuthProvider"
+import { useConnect } from "../utils/useGoogleGsiClient"
+import H1 from "../components/basics/H1"
+import Paragraph from "../components/basics/Paragraph"
 
 const Thomas = () => {
-	return <HelloName name="Thomas" />
+	// On appel le context "AuthContext" pour y stocker le userToken
+	const { userToken, setUserToken } = React.useContext(AuthContext)
+
+	// On fait une demande de connexion One Tap à google
+	// Todo: Ajouter la gestion de session
+	const user = useConnect(process.env.REACT_APP_GOOGLE_CLIENT_ID, true, false)
+
+	// On stock le userToken dans le contexte (pour utilisation ailleur dans le projet)
+	React.useEffect(() => {
+		if (userToken) return
+		setUserToken(user)
+	}, [setUserToken, user, userToken])
+
+	return (
+		<>
+			<H1>GSI Client Google</H1>
+			<br />
+			<Paragraph>Connexion à l'aide de la nouvelle API de Google</Paragraph>
+			<br />
+			<Paragraph>userToken: {userToken ?? "non connecté"}</Paragraph>
+			<br />
+			{userToken ? <Paragraph>Connexion OK (Todo: déconnexion)</Paragraph> : <div id="buttonDiv"></div>}
+		</>
+	)
 }
 
 export default Thomas
-
-// Exemple avec des props définis
-type UnComposantProps = {
-	unTexte: string
-	unTexteOptionnel?: string
-	unNombre: number
-	unTableau: []
-	unObjet: {}
-	unTableauDeNombres: number[]
-	nimporteQuoi: any
-}
-export const UnComposant = ({ unTexte }: UnComposantProps) => <>{unTexte}</>
